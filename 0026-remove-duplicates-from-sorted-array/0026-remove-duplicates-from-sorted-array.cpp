6c92b7df8382df7fea3d28007fd5c43f314136b3
nishant jain <83751330+Nishant-Jain-Codes@users.noreply.github.com>
2024-07-17 16:14:18 +0530

    Time: 3 ms (96.28%), Space: 21.4 MB (16.11%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -1,23 +1,55 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        if(nums.size()<=1 )
+        // Edge case: if the array has 0 or 1 element, no duplicates to remove
+        if(nums.size() <= 1) {
             return nums.size();
-            
-        int i =0;
-        int j = 0;
-        while(j<nums.size()-1){
-            while(j<nums.size()-1 && nums[j]==nums[j+1]){
-                j++;
+        }
+        
+        int slow = 0; // slow pointer to track unique elements
+        int fast = 1; // fast pointer to iterate through the array
+        
+        while(fast < nums.size()) {
+            // If current element is different from previous unique element
+            if(nums[fast] != nums[slow]) {
+                // Move slow pointer forward
+                slow++;
+                // Update the current slow pointer position with the unique element
+                nums[slow] = nums[fast];
             }
-            nums[i]=nums[j];
-            j++;
-            i++;
+            // Move fast pointer forward
+            fast++;
         }
-        if(j== nums.size()-1 && nums[j]!=nums[j-1]){
-            nums[i] = nums[j];
-            i++;
-        }
-        return i;
+        
+        // Return the length of the array up to the last unique element found
+        return slow + 1;
     }
 };
+
+
+// class Solution {
+// public:
+// // two pointer approach 
+// // time O(N)
+// // space O(1);
+//     int removeDuplicates(vector<int>& nums) {
+//         if(nums.size()<=1 )
+//             return nums.size();
+            
+//         int i =0;
+//         int j = 0;
+//         while(j<nums.size()-1){
+//             while(j<nums.size()-1 && nums[j]==nums[j+1]){
+//                 j++;
+//             }
+//             nums[i]=nums[j];
+//             j++;
+//             i++;
+//         }
+//         if(j== nums.size()-1 && nums[j]!=nums[j-1]){
+//             nums[i] = nums[j];
+//             i++;
+//         }
+//         return i;
+//     }
+// };
