19eca8e71a886a01c416bbd36328616d649cef65
nishant jain <83751330+Nishant-Jain-Codes@users.noreply.github.com>
2024-07-19 16:19:36 +0530

    Time: 62 ms (13.68%), Space: 21.3 MB (70.47%) - LeetHub

--- 0680-valid-palindrome-ii/0680-valid-palindrome-ii.cpp
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.cpp
@@ -1,36 +1,20 @@
 class Solution {
 public:
+    bool recursion(string & s , int start , int end){
+        if(start>=end)
+            return true;
+        if(s[start]==s[end])
+            return recursion(s,start+1,end-1);
+        if(start - 0 != s.length() - end - 1)
+            return false;
+        bool ans = false;
+        if(s[start+1] == s[end])
+            ans |= recursion(s,start+2,end-1);
+        if(s[start] == s[end-1])
+            ans|= recursion(s,start+1,end-2);
+        return ans;
+    }
     bool validPalindrome(string s) {
-        //move from left and right pointer 
-        // if unmatch check if its the first unmatch 
-        // check if left+1 matches right or right-1 matches left i.e we are skipping left and right respectively , if none match the return false 
-        // 
-        int len = s.length();
-        int start = 0;
-        int end = len-1;
-        while(start<end){
-            if(s[start]==s[end]){
-                start++;
-                end--;
-            }
-            else{
-                if(start - 0 == len - end - 1) //if left and right size is equal i.e we didnt skip anything
-                {
-                    if(s[start+1]==s[end]){ // skip left elelemnt
-                        start++;
-                    }
-                    else if(s[end-1]==s[start]){ //skip right elelemnt
-                        end--;
-                    }
-                    else{
-                        return false;
-                    }
-                }
-                else
-                    return false;
-            }
-
-        }
-        return true;
+        return recursion(s,0,s.length()-1);
     }
 };
