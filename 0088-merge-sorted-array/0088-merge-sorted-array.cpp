08b4a34eb2e5656e29bd3408dab69c6d7061255b
nishant jain <83751330+Nishant-Jain-Codes@users.noreply.github.com>
2024-07-19 15:27:37 +0530

    Time: 5 ms (28.16%), Space: 11.2 MB (9.04%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -1,6 +1,34 @@
 class Solution {
 public:
-    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+    // time : O(n+m)
+    // space : O(1)
+    //sort from the back i.e largest elements first and sort the array inplace
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n){
+        int left = m-1;
+        int right = n-1;
+        int tempidx = m+n-1;
+        while(left>=0 && right>=0){
+            if(nums1[left]>=nums2[right]){
+                nums1[tempidx] = nums1[left];
+                left--;
+                tempidx--;
+            }
+            else{
+                nums1[tempidx] = nums2[right];
+                right--;
+                tempidx--;
+            }
+        }
+        while(right>=0)
+            {
+                nums1[tempidx] = nums2[right];
+                right--;
+                tempidx--;
+            }
+    }
+    // time : O(n+m)
+    // space : O(n+m)
+    void merge_less_optimised(vector<int>& nums1, int m, vector<int>& nums2, int n) {
         vector<int> temp(n+m) ; 
         int li = 0;
         int ri = 0 ;
