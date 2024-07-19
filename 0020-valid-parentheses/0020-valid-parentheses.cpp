39f0119a62de82171c4cde2d4dac52a8abb794cb
nishant jain <83751330+Nishant-Jain-Codes@users.noreply.github.com>
2024-07-19 13:43:29 +0530

    Time: 4 ms (11.84%), Space: 8.3 MB (8.2%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -1,6 +1,29 @@
+
+
 class Solution {
 public:
-    bool isValid(string s) {
+
+    // time : O(N)
+    // space : O(N)
+    bool isValid(string s){
+        stack<char> stk;
+        unordered_map<char,char> bracketPair = {{')','('},{'}','{'},{']','['}};
+        for( auto ch : s){
+            if(bracketPair.find(ch)!=bracketPair.end()){
+                if(stk.empty() || stk.top() != bracketPair[ch])
+                    return false;
+                else 
+                    stk.pop();
+            }
+            else
+                stk.push(ch);
+        }
+        return stk.empty();
+    }
+
+    // time : O(N)
+    // space : O(N)
+    bool isValid_poorlyWrittenCode(string s) {
         stack<char> stk;
         for(int i = 0;i<s.length();i++){
             if(s[i]=='(' || s[i]=='{' || s[i]=='[')
