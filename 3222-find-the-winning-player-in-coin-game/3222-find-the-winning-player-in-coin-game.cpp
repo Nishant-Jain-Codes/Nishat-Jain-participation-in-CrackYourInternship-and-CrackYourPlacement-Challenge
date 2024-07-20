e1b8c785c8c11008df1013f8f0acd424165e3fae
nishant jain <83751330+Nishant-Jain-Codes@users.noreply.github.com>
2024-07-20 21:31:58 +0530

    Time: 4 ms (100%), Space: 7.7 MB (25%) - LeetHub

---
+++ 3222-find-the-winning-player-in-coin-game/3222-find-the-winning-player-in-coin-game.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int recursion(int x , int y ,int curP){
+        if(x>=1 && y>=4)
+            return recursion(x-1,y-4,!curP);
+        else
+            return !curP;
+    }
+    string losingPlayer(int x, int y) {
+        // int winner = 0; // 1-> alice;
+        int winner =  recursion(x,y,0);
+        return winner == 0 ? "Alice" : "Bob";
+    }
+
+};
