956a8303455132e11170eaed8648bd56e28a84ef
nishant jain <83751330+Nishant-Jain-Codes@users.noreply.github.com>
2024-07-20 22:15:28 +0530

    Time: 24 ms (79.02%), Space: 26.4 MB (18.32%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,12 +1,24 @@
 class Solution {
 public:
-    // time : O(N)
+    // time : O(N*M) , M : size of largest string , N : total Strings
     // space : O(N)
+    string countSort(string & str){
+        int hash[26] = {0};
+        for(auto &x : str)
+            hash[x-'a']++;
+        string output ="";
+        for(int i=0;i<26;i++){
+            for(int j = 0;j<hash[i];j++){
+                output+= ('a'+i);
+            }
+        }
+        return output;
+    }
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
         unordered_map<string,vector<string>> map;
         for(auto str : strs){
-            string curStr = str;
-            sort(curStr.begin(),curStr.end());
+            string curStr = countSort(str);
+            
             map[curStr].push_back(str);
         }
         vector<vector<string>> ans;
