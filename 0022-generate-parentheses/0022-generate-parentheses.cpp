b528f60449691eb59f4e6dc71c8695d69598dae2
nishant jain <83751330+Nishant-Jain-Codes@users.noreply.github.com>
2024-07-21 01:07:46 +0530

    Time: 7 ms (22.89%), Space: 14.7 MB (72.14%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.cpp
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -1,26 +1,21 @@
 class Solution {
 public:
-    // time compleixty : O()
-    // space compleixty : O()
-    void helper(int open , int close , vector<string>& answer , string & curStr){
+    // time compleixty : O(2^2n)
+    // space compleixty : O(n) {auxilary stack space}
+    void dfs(int open , int close , vector<string>& answer , string & curStr){
         if(close == 0){
             string curAns = curStr;
             answer.push_back(curAns);
             return ;
         }
-        if(open == close){
+        if(open >0){
             curStr.push_back('(');
-            helper(open-1,close,answer,curStr);
+            dfs(open-1,close,answer,curStr);
             curStr.pop_back();
         }
-        else{
-            if(open>0){
-                curStr.push_back('(');
-                helper(open-1,close,answer,curStr);
-                curStr.pop_back();
-            }
+        if(open < close){
             curStr.push_back(')');
-            helper(open,close-1,answer,curStr);
+            dfs(open,close-1,answer,curStr);
             curStr.pop_back();
                 
         }
@@ -29,7 +24,7 @@ public:
     vector<string> generateParenthesis(int n) {
         vector<string> answer;
         string ans = "";
-        helper(n,n,answer,ans);
+        dfs(n,n,answer,ans);
         return answer;
     }
 };
