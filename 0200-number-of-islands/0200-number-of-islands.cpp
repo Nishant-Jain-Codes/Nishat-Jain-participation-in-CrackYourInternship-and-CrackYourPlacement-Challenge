c0b002f0b6f8cf0ad8c4db3cefcbb8bd6966ffc3
nishant jain <83751330+Nishant-Jain-Codes@users.noreply.github.com>
2024-07-21 20:17:36 +0530

    Time: 29 ms (54.55%), Space: 16.3 MB (57.92%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,33 +1,43 @@
 class Solution {
 public:
-    void dfs(vector<vector<char>> & grid , vector<vector<bool>> & visited , int row , int col){
+    // time : O(N*M)
+    // space : O(min(N,M)) {auxiliary Stack Space}
+    void dfs(vector<vector<char>>& grid, vector<vector<bool>>& visited, int row, int col) {
         int m = grid.size();
         int n = grid[0].size();
-        if(row>=m || col>=n || row<0 || col<0)
+        if (row < 0 || col < 0 || row >= m || col >= n || grid[row][col] == '0' || visited[row][col])
             return;
-        if(grid[row][col]=='0' || visited[row][col])
-            return ;
-        if(grid[row][col]=='1'){
-            visited[row][col] = true;
-            dfs(grid,visited,row-1,col);
-            dfs(grid,visited,row+1,col);
-            dfs(grid,visited,row,col-1);
-            dfs(grid,visited,row,col+1);
+        
+        visited[row][col] = true;
+        
+        int directions[4][2] = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+        //up,down,left,right
+        for (int i = 0; i < 4; ++i) {
+            int newRow = row + directions[i][0];
+            int newCol = col + directions[i][1];
+            dfs(grid, visited, newRow, newCol);
         }
     }
+    
     int numIslands(vector<vector<char>>& grid) {
         int m = grid.size();
         int n = grid[0].size();
-        vector<vector<bool>> visited(m,vector<bool>(n,false));
-        int ans = 0;
-        for(int i =0;i<m;i++){
-            for(int j = 0 ; j<n;j++){
-                if(grid[i][j]=='1' && visited[i][j] == false){
-                    ans++;
-                    dfs(grid,visited,i,j);
+        
+        if (m == 0 || n == 0)
+            return 0;
+        
+        vector<vector<bool>> visited(m, vector<bool>(n, false));
+        int numberOfIslands = 0;
+        
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (grid[i][j] == '1' && !visited[i][j]) {
+                    ++numberOfIslands;
+                    dfs(grid, visited, i, j);
                 }
             }
         }
-        return ans;
+        
+        return numberOfIslands;
     }
 };
