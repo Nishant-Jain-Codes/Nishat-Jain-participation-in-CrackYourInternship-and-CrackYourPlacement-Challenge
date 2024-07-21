8fd71e90a330c2bf67315115479dfc10343a52c2
nishant jain <83751330+Nishant-Jain-Codes@users.noreply.github.com>
2024-07-21 20:28:24 +0530

    Time: 27 ms (70.16%), Space: 16 MB (64.77%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -2,20 +2,20 @@ class Solution {
 public:
     // time : O(N*M)
     // space : O(min(N,M)) {auxiliary Stack Space}
-    void dfs(vector<vector<char>>& grid, vector<vector<bool>>& visited, int row, int col) {
+    void dfs(vector<vector<char>>& grid, int row, int col) {
         int m = grid.size();
         int n = grid[0].size();
-        if (row < 0 || col < 0 || row >= m || col >= n || grid[row][col] == '0' || visited[row][col])
+        if (row < 0 || col < 0 || row >= m || col >= n || grid[row][col] !='1')
             return;
         
-        visited[row][col] = true;
+        grid[row][col] = '2';
         
         int directions[4][2] = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
         //up,down,left,right
         for (int i = 0; i < 4; ++i) {
             int newRow = row + directions[i][0];
             int newCol = col + directions[i][1];
-            dfs(grid, visited, newRow, newCol);
+            dfs(grid, newRow, newCol);
         }
     }
     
@@ -26,14 +26,13 @@ public:
         if (m == 0 || n == 0)
             return 0;
         
-        vector<vector<bool>> visited(m, vector<bool>(n, false));
         int numberOfIslands = 0;
         
         for (int i = 0; i < m; ++i) {
             for (int j = 0; j < n; ++j) {
-                if (grid[i][j] == '1' && !visited[i][j]) {
+                if (grid[i][j] == '1') {
                     ++numberOfIslands;
-                    dfs(grid, visited, i, j);
+                    dfs(grid,i, j);
                 }
             }
         }
