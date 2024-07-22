560e45f6c393e60710297cc57ca571d6e7996f19
nishant jain <83751330+Nishant-Jain-Codes@users.noreply.github.com>
2024-07-22 20:35:41 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    bool isValidLocation(vector<vector<int>>& image,int orignalColor , int row ,int col){
+        int m = image.size();
+        int n = image[0].size();
+        if(row<0 || col<0 || row>=m || col>=m || image[row][col]!=orignalColor)
+            return false;
+        return true;
+    }
+    void dfs(vector<vector<int>>& image , int row ,int col , int orignalColor , int targetColor){
+        if(!isValidLocation(image,orignalColor,row,col))
+            return;
+        image[row][col] = targetColor;
+        vector<pair<int,int>> directions = {{-1,0},{1,0},{0,-1},{0,1}};
+        for(auto &x : directions){
+            int newRow = row + x.first;
+            int newCol = col + x.second;
+            dfs(image, newRow,newCol, orignalColor , targetColor);
+        }
+    }
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
+        int m = image.size();
+        int n = image[0].size();
+        if(sr>=m || sc>=n)
+            return image;
+        if(color == image[sr][sc])
+            return image;
+        dfs(image,sr,sc,image[sr][sc],color);
+        // for(int i = 0 ;i<m;i++){
+        //     for(int j = 0;j<n;j++){
+        //         if(image[i][j]<0)
+        //             image[i][j] = abs(image[i][j]);
+        //     }
+        // }
+        return image;
+    }
+};
