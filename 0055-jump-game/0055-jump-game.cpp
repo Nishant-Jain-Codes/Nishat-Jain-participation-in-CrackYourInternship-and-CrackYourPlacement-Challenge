813159c26a85cde46cee055e9fb4184eb7e9142a
nishant jain <83751330+Nishant-Jain-Codes@users.noreply.github.com>
2024-07-24 01:38:02 +0530

    Time: 32 ms (95.68%), Space: 51.1 MB (32.41%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
-    bool helper(vector<int>& nums, int curIdx , vector<int>& dp){
+    //time O(N^2)
+    //space O(N)
+    bool recursion(vector<int>& nums, int curIdx , vector<int>& dp){
         if(curIdx >= nums.size()-1)
             return true;
         if(dp[curIdx]!=-1)
@@ -9,16 +11,26 @@ public:
         int canReach = false;
         if(curStrength+curIdx >= nums.size()-1)
             return dp[curIdx]=true;
-        for(int i= curIdx ; i<= curIdx+curStrength ;i++){
-            if(i!=curIdx)
-                if(helper(nums,i,dp))
+        for(int i= curIdx+1 ; i<= curIdx+curStrength ;i++){
+                if(recursion(nums,i,dp))
                     return dp[curIdx]=true;
         }
         return dp[curIdx]=false;
     }
 
     bool canJump(vector<int>& nums) {
-        vector<int> dp(nums.size(),-1);
-        return helper(nums,0,dp);
+        // vector<int> dp(nums.size(),-1);
+        // return helper(nums,0,dp);
+        //Optimised solution GREEDY 
+        // Time : O(N)
+        // Space : O(1)
+        int curMaxDest = 0;
+        for(int i = 0;i<nums.size();i++){
+            if(curMaxDest<i)
+                return false;
+            curMaxDest = max(curMaxDest , nums[i]+i);       if(curMaxDest>=nums.size())
+                return true;
+        }
+        return true;
     }
 };
