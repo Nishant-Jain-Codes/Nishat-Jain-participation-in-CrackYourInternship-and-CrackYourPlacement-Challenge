092d53319c13086492733187700ff1afa7e3d34c
nishant jain <83751330+Nishant-Jain-Codes@users.noreply.github.com>
2024-07-24 02:27:26 +0530

    Time: 3 ms (29.48%), Space: 7.5 MB (22.59%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    // time O(N)
+    // space O(1)
+    int climbStairs(int n) {
+        // vector<int> dp(n+1,0);
+        // dp[0]=1;
+        // dp[1]=1;
+        int p_2 = 1;
+        int p_1 = 1;
+        int cur = 1;
+        for(int i = 2; i<=n;i++){
+            // dp[i] = dp[i-1]+dp[i-2];
+            cur = p_1 + p_2;
+            p_2 = p_1;
+            p_1 = cur;
+        }
+        return cur;
+    }
+};
