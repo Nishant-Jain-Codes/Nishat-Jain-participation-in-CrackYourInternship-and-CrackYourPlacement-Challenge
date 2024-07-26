2a4d591f6a8714dbce41ea567b8bda0cdc6b9735
nishant jain <83751330+Nishant-Jain-Codes@users.noreply.github.com>
2024-07-26 05:42:11 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0018-4sum/0018-4sum.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    // time : O(N^3)
+    // space : O(1)
+    vector<vector<int>> fourSum(vector<int>& nums, int target) {
+        if(nums.size()<=3)
+            return {};
+        vector<vector<int>> answer;
+        sort(nums.begin(),nums.end());
+        for(int i =0;i<nums.size()-3;i++){
+            if(i>0 && nums[i]==nums[i-1])
+                continue;
+                target=target-nums[i];
+            for(int j = i+1;j<nums.size()-2;j++){
+                if(j>i+1 && nums[j]==nums[j-1])
+                    continue;
+                int si = j+1;
+                int ei = nums.size()-1;
+                target=target-nums[j];
+                while(si<ei){
+                    int sum =+nums[si]+nums[ei];
+                    if(sum==target){
+                        answer.push_back({nums[i],nums[j],nums[si],nums[ei]});
+                        if(nums[si]==nums[ei])
+                            break;
+                        else{
+                            while(nums[si]==nums[si+1])
+                                si++;
+                            while(nums[ei]==nums[ei-1])
+                                ei--;
+                            si++;
+                            ei--;
+                        }
+                    }
+                    else if (sum<target){
+                        si++;
+                    }
+                    else{
+                        ei--;
+                    }
+                }
+                target=target+nums[j];
+            }
+            target=target+nums[i];
+        }
+        return answer;
+    }
+};
