a9909a6b853b9fdd4e1bfcb9fd9598ab2f1d953a
nishant jain <83751330+Nishant-Jain-Codes@users.noreply.github.com>
2024-07-29 23:28:57 +0530

    Time: 34 ms (84.27%), Space: 17.4 MB (22.6%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
@@ -9,6 +9,8 @@
 class Solution {
 public:
     //brute force (using hashing)
+    //Time : O(N+M)
+    //Space : o(N+M)
     ListNode *bruteForce(ListNode *headA , ListNode *headB){
         unordered_set<ListNode*> set;
         for(ListNode* temp = headA ; temp!=NULL ; temp = temp->next){
@@ -22,7 +24,38 @@ public:
         }
         return NULL;
     }
+    int length(ListNode*head){
+        int l = 0;
+        ListNode* temp = head;
+        while(temp){
+            temp=temp->next;
+            l++;
+        }
+        return l;
+    }
+    ListNode *optimised(ListNode *headA, ListNode *headB){
+        int l1 = length(headA);
+        int l2 = length(headB);
+        ListNode* big = headA;
+        ListNode* sml = headB;
+        if(l1<l2)
+            {
+                swap(big,sml);
+                swap(l1,l2);
+            }
+        int diff = l1-l2;
+        while(diff){
+            big=big->next;
+            diff--;
+        }
+        while(big!=sml){
+            big=big->next;
+            sml=sml->next;
+        }
+        return big==NULL? NULL:big;
+    }
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
-        return bruteForce(headA,headB);
+        // return bruteForce(headA,headB);
+        return optimised(headA,headB);
     }
 };
