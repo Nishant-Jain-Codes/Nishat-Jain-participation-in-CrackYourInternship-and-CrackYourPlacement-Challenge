c01cce74f2e5080fff451dd20f6c5eafbcff775f
nishant jain <83751330+Nishant-Jain-Codes@users.noreply.github.com>
2024-07-30 03:21:13 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0974-subarray-sums-divisible-by-k/0974-subarray-sums-divisible-by-k.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+    //brute force 
+    // time O(N^2)
+    // space O(1)
+    //gettting tle
+    int bruteForce(vector<int>& nums, int k){
+        int ans = 0;
+        for(int i = 0 ; i < nums.size();i++){
+            int curSum = 0;
+            for(int j = i;j<nums.size();j++){
+                curSum+=nums[j];
+                if(curSum%k==0)
+                    {
+                // cout<<"cs "<<curSum<<endl;
+                        // cout<<i<<" "<<j<<endl;    
+                        ans++;
+                        
+                    }
+            }     
+        }
+        return ans;
+    }
+    // time : O(N)
+    // space : O(N)
+    int optimised(vector<int>& nums,int k){
+        unordered_map<int,int> map;
+        int curSum = 0;
+        int curCount = 0;
+        map[0]=1;
+        for(int i = 0;i<nums.size();i++){
+            curSum+=nums[i];
+            // remove subarray with the same remainder i.e curSum%k , so that after removing we get 0 remainder , thus the resultant subarray will be a factor of k 
+            int remove = (curSum)%k; 
+            cout<<remove<<" ";
+            curCount+=map[remove];
+            map[curSum%k]++;
+        }
+        cout<<endl;
+        for(auto [f,s] : map){
+
+            cout<<f<<" "<<s<<endl;
+        }
+        return curCount;
+    }
+    int subarraysDivByK(vector<int>& nums, int k) {
+        // return bruteForce(nums,k);
+        return optimised(nums,k);
+    }
+};
