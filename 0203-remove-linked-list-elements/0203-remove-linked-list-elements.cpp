85d271081572d042070c698db93d702d0cf783eb
nishant jain <83751330+Nishant-Jain-Codes@users.noreply.github.com>
2024-07-29 17:12:32 +0530

    Time: 11 ms (88.54%), Space: 19.9 MB (22.67%) - LeetHub

--- 0203-remove-linked-list-elements/0203-remove-linked-list-elements.cpp
+++ 0203-remove-linked-list-elements/0203-remove-linked-list-elements.cpp
@@ -10,21 +10,34 @@
  */
 class Solution {
 public:
+    //iterative
     ListNode* removeElements(ListNode* head, int val) {
-        if(!head)
-            return NULL;
-        
-        if(head->val == val){
-            ListNode* del = head;
-            head=head->next;
-            del->next = NULL;
-            delete del;
-            return removeElements(head,val);
-        }
-        else
-            {
-                head->next = removeElements(head->next,val);
-                return head;
+        ListNode dummy(0,head);
+        ListNode* prev = &dummy;
+        for(ListNode* temp= head ; temp!=NULL;temp=temp->next){
+            if(temp->val!=val){
+                prev->next = temp;
+                prev = prev->next;
             }
+        }
+        prev->next = NULL;
+        return dummy.next;
     }
+    // ListNode* removeElements(ListNode* head, int val) {
+    //     if(!head)
+    //         return NULL;
+        
+    //     if(head->val == val){
+    //         ListNode* del = head;
+    //         head=head->next;
+    //         del->next = NULL;
+    //         delete del;
+    //         return removeElements(head,val);
+    //     }
+    //     else
+    //         {
+    //             head->next = removeElements(head->next,val);
+    //             return head;
+    //         }
+    // }
 };
