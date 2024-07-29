35075c53708a84ebdb33e0115c4cf0f93dff4086
nishant jain <83751330+Nishant-Jain-Codes@users.noreply.github.com>
2024-07-30 02:57:00 +0530

    Time: 66 ms (69.19%), Space: 51 MB (17.41%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -1,21 +1,17 @@
 class Solution {
 public:
+    //time : O(N)
+    //space : O(N)
     int subarraySum(vector<int>& nums, int k) {
-        unordered_map<int,pair<int,int>> map;//<prefuxSum,freq> 
-        map[0] = {1,-1};
+        unordered_map<int,int> map;//<prefuxSum,freq> 
+        map[0] = 1;
         int curSum = 0;
         int ans = 0;
-        for(int i =0;i<=nums.size();i++){
-            if(i<nums.size())
+        for(int i =0;i<nums.size();i++){
             curSum+=nums[i];
-            else if(k==0)
-                continue;
-            int target = curSum-k;
-            if(map.find(target)!=map.end()){
-                ans+=map[target].first;
-            }
-            map[target].first++;
-            map[target].second = i;
+            int remove = curSum-k;
+            ans+=map[remove];
+            map[curSum]++;
         }
         return ans;
     }
