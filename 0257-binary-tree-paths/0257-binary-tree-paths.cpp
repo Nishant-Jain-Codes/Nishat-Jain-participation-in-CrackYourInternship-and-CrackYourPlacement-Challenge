0a54678d36a20ce3ba1034380a3f10863c44ab71
nishant jain <83751330+Nishant-Jain-Codes@users.noreply.github.com>
2024-07-31 15:43:05 +0530

    Time: 4 ms (49.44%), Space: 16.9 MB (39.81%) - LeetHub

--- 0257-binary-tree-paths/0257-binary-tree-paths.cpp
+++ 0257-binary-tree-paths/0257-binary-tree-paths.cpp
@@ -1,48 +1,26 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
-    void helper(TreeNode* root ,vector<string> &allPaths,string & curPath){
-        if(!root)
-        {
-            string addPath(curPath);
-            allPaths.push_back(addPath);
-            return ;
+    void helper(TreeNode* root, vector<string>& allPaths, string curPath) {
+        if (!root) return;
+
+        if (!curPath.empty()) {
+            curPath += "->";
         }
-        string rootVal = to_string(root->val);
-        string prevString = curPath;
-        curPath+=rootVal;
-        if(!root->left && !root->right)
-            helper(root->left,allPaths,curPath);
-        if(root->left){
-            curPath.push_back('-');
-            curPath.push_back('>');
-            helper(root->left,allPaths,curPath);
-            curPath.pop_back();
-            curPath.pop_back();
+        curPath += to_string(root->val);
+
+        if (!root->left && !root->right) {
+            allPaths.push_back(curPath);
+        } else {
+            if (root->left) helper(root->left, allPaths, curPath);
+            if (root->right) helper(root->right, allPaths, curPath);
         }
-        if(root->right){
-            curPath.push_back('-');
-            curPath.push_back('>');
-            helper(root->right,allPaths,curPath);
-            curPath.pop_back();
-            curPath.pop_back();
-        }
-        curPath = prevString;
     }
+
     vector<string> binaryTreePaths(TreeNode* root) {
         vector<string> allPaths;
-        string curPath = "";
-        helper(root,allPaths,curPath);
+        if (root) {
+            helper(root, allPaths, "");
+        }
         return allPaths;
     }
 };
