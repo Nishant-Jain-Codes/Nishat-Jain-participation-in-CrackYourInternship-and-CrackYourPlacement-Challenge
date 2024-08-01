19b10b86b5c8432fe8b036783f1f9d8976ab7bed
nishant jain <83751330+Nishant-Jain-Codes@users.noreply.github.com>
2024-08-01 08:49:49 +0530

    Time: 0 ms (100%), Space: 17.6 MB (16.71%) - LeetHub

--- 2678-number-of-senior-citizens/2678-number-of-senior-citizens.cpp
+++ 2678-number-of-senior-citizens/2678-number-of-senior-citizens.cpp
@@ -2,10 +2,12 @@ class Solution {
 public:
     int countSeniors(vector<string>& details) {
         int count=0;
-        for(auto x : details){
-            string age = x.substr(11,2);
-            cout<<age<<" ";
-            int ageN = stoi(age);
+        for(auto &x : details){
+            // string age = x.substr(11,2);
+            int first = x[11]-'0';
+            int second = x[12]-'0';
+            int ageN = first*10 + second;
+            cout<<ageN<<" ";
             if(ageN>60)
                 count++;
         }
