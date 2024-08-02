6e47f20ad0bb692c345e425e6a7fe6abfac45b10
nishant jain <83751330+Nishant-Jain-Codes@users.noreply.github.com>
2024-08-02 22:28:54 +0530

    Time: 70 ms (48.72%), Space: 83.1 MB (57.56%) - LeetHub

--- 2134-minimum-swaps-to-group-all-1s-together-ii/2134-minimum-swaps-to-group-all-1s-together-ii.cpp
+++ 2134-minimum-swaps-to-group-all-1s-together-ii/2134-minimum-swaps-to-group-all-1s-together-ii.cpp
@@ -1,33 +1,24 @@
 class Solution {
 public:
     int sw(vector<int>& nums,int k ){
-        int count = 0;
-        for(auto &x : nums){
-            if(x==k)
-                count++;
+        int totalK = 0;
+        for(auto & x : nums)
+            if(x == k) 
+                totalK++;
+        int nonKcount = 0;
+        for(int i =0;i<totalK;i++)
+            if(nums[i]!=k)
+                nonKcount++;
+        int minSwaps  = nonKcount;
+        for(int i = totalK ; i<nums.size();i++){
+            if(nums[i]!=k) nonKcount++;
+            if(nums[i-totalK]!=k) nonKcount--;
+            minSwaps = min(minSwaps,nonKcount);
         }
-        int kCount = 0;
-        int notKCount = 0;
-        for(int i =0;i<count;i++){
-            if(nums[i]==k)
-                kCount++;
-            else
-                notKCount++;
-        }
-        int ans = notKCount;
-        for(int i = count;i<nums.size();i++){
-            if(nums[i]==k)
-                kCount++;
-            else
-                notKCount++;
-            if(nums[i-count] == k)
-                kCount--;
-            else
-                notKCount--;
-            ans = min(ans,notKCount);
-        }
-        return ans;
+        return minSwaps;
     }
+    //time : O(N)
+    //space : O(1)
     int minSwaps(vector<int>& nums) {
         return min(sw(nums,1),sw(nums,0));
     }
