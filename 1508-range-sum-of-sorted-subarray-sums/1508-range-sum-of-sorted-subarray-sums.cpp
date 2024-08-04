0b4660c4f6fe84df6bf00c565273b2bbde778bbf
nishant jain <83751330+Nishant-Jain-Codes@users.noreply.github.com>
2024-08-04 09:28:10 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 1508-range-sum-of-sorted-subarray-sums/1508-range-sum-of-sorted-subarray-sums.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int rangeSum(vector<int>& nums, int n, int left, int right) {
+        vector<int> sums ;
+        for(int i = 0;i<nums.size();i++){
+            int curSum = 0;
+            for(int j = i;j<nums.size();j++){
+                curSum+= nums[j];
+                sums.push_back(curSum);
+            }
+        }
+        for(auto & x : sums)
+            cout<<x<<" ";
+        cout<<endl;
+        sort(sums.begin(),sums.end());
+        long long int ans =0 ;
+        int mod = 1e9 + 7;
+        for(auto & x : sums)
+            cout<<x<<" ";
+        cout<<endl;
+        // int ans = 0;
+        for(int i = left-1;i<right;i++)
+        {
+            cout<<sums[i]<<" ";
+            ans = (ans+sums[i])%mod;
+        }
+        return ans;
+    }
+};
