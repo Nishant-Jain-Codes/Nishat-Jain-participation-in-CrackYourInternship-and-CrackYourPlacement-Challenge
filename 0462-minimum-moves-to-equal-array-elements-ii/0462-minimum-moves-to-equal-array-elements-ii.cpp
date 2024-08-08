fc442a9e7adac7580cd7fe19282427706b5c919e
nishant jain <83751330+Nishant-Jain-Codes@users.noreply.github.com>
2024-08-08 10:03:05 +0530

    Time: 9 ms (59.79%), Space: 14.9 MB (12.3%) - LeetHub

--- 0462-minimum-moves-to-equal-array-elements-ii/0462-minimum-moves-to-equal-array-elements-ii.cpp
+++ 0462-minimum-moves-to-equal-array-elements-ii/0462-minimum-moves-to-equal-array-elements-ii.cpp
@@ -1,24 +1,17 @@
 class Solution {
 public:
-    int movesTox(vector<int>& nums , int x ){
-        int ans = 0;
-        for(auto a : nums){
-            ans+= abs(a-x);
-        }
-        return ans;
-    }
+    // (maths property) median minimses the sum of absolute differences 
+    //time : O(N)
+    //space : O(1)
     int minMoves2(vector<int>& nums) {
-        int sum = 0;
-        // int ans = INT_MAX;
-        // for(auto a : nums)
-        //     sum+=a;
-            // ans = min(ans,movesTox(nums,a));
-        // return ans;
         int n = nums.size();
         sort(nums.begin(),nums.end());
-        if(n%2 != 0)
-            return movesTox(nums,nums[n/2]);
-        else 
-            return min(movesTox(nums,nums[n/2]),movesTox(nums,nums[n/2 -1]));
+        int median = nums[n/2];
+        int ans = 0;
+        //in case of even size also , either median value will give the minimsed difference
+        for(auto a : nums){
+            ans+= abs(a-median);
+        }
+        return ans;
     }
 };
