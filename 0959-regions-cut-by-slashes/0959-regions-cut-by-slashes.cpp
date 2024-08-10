0c16e63b31f95bfa08f5f4d9d80b18b7d0f099e0
nishant jain <83751330+Nishant-Jain-Codes@users.noreply.github.com>
2024-08-11 05:17:34 +0530

    Time: 14 ms (26.89%), Space: 16.9 MB (13.84%) - LeetHub

---
+++ 0959-regions-cut-by-slashes/0959-regions-cut-by-slashes.cpp
@@ -0,0 +1,56 @@
+class Solution {
+public:
+    int regionsBySlashes(vector<string>& grid) {
+        return approach1(grid);
+    }
+private: 
+    // time : O(9*n^2)
+    // space : O(9*n^2)
+    // approach 1
+    int approach1(vector<string>& grid) {
+        int n = grid.size();
+        vector<vector<int>> expandedGrid(3*n , vector<int>(3*n,0));
+        for(int r = 0;r<n;r++){
+            for(int c = 0;c<n;c++){
+                if(grid[r][c] == '\\'){
+                    expandedGrid[3*r][3*c] = 1;
+                    expandedGrid[3*r + 1][3*c + 1] = 1;
+                    expandedGrid[3*r + 2][3*c + 2] = 1;
+                }
+                else if(grid[r][c] == '/'){
+                    expandedGrid[3*r][3*c + 2] = 1;
+                    expandedGrid[3*r + 1][3*c + 1] = 1;
+                    expandedGrid[3*r + 2][3*c] = 1;
+                }
+                
+            }
+        }
+        queue<pair<int,int>> q ; 
+        int m = expandedGrid.size();
+        int parts = 0;
+        vector<pair<int,int>> directions = {{-1,0},{1,0},{0,-1},{0,1}};
+        for(int i = 0;i<m;i++){
+            for(int j = 0;j<m;j++){
+                if(expandedGrid[i][j] == 0){
+                    parts++;
+                    q.push({i,j});
+                    expandedGrid[i][j] = 2;
+                    while(!q.empty()){
+                        int x = q.front().first;
+                        int y = q.front().second;
+                        q.pop();
+                        for(auto &dir : directions){
+                            int x_new = x + dir.first;
+                            int y_new = y + dir.second;
+                            if(x_new >= 0 && x_new<m && y_new >= 0 && y_new < m && expandedGrid[x_new][y_new]==0){
+                                expandedGrid[x_new][y_new] = 2;
+                                q.push({x_new,y_new});
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return parts;
+    }
+};
