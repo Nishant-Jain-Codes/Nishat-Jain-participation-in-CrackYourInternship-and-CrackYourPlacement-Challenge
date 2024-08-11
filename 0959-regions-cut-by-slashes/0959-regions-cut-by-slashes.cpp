d7feef8b4f8061c64b13417416add36dcb781bfb
nishant jain <83751330+Nishant-Jain-Codes@users.noreply.github.com>
2024-08-11 05:30:40 +0530

    Time: 67 ms (6.79%), Space: 16.9 MB (14.1%) - LeetHub

--- 0959-regions-cut-by-slashes/0959-regions-cut-by-slashes.cpp
+++ 0959-regions-cut-by-slashes/0959-regions-cut-by-slashes.cpp
@@ -7,6 +7,12 @@ private:
     // time : O(9*n^2)
     // space : O(9*n^2)
     // approach 1
+        //convert each cell of input into a 3x3 grid 
+        //'\' matrix with primary diagonal = 1
+        // '//' matrix with seconary diagonal = 1
+        // ' ' means null matrix 
+        // now we'll get a (3n)*(3n) matrix representation of graph 
+        // check number of connected components using bfs / dfs 
     int approach1(vector<string>& grid) {
         int n = grid.size();
         vector<vector<int>> expandedGrid(3*n , vector<int>(3*n,0));
@@ -27,6 +33,12 @@ private:
         }
         queue<pair<int,int>> q ; 
         int m = expandedGrid.size();
+        for(int i = 0;i<m;i++){
+            for(int j = 0;j<m;j++){
+                cout<<expandedGrid[i][j]<<" ";
+            }
+            cout<<endl;
+        }
         int parts = 0;
         vector<pair<int,int>> directions = {{-1,0},{1,0},{0,-1},{0,1}};
         for(int i = 0;i<m;i++){
