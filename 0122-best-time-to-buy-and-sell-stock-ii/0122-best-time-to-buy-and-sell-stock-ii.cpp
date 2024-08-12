2ba32a2a1c878e8c4cc59df433e94fb8a5b30431
nishant jain <83751330+Nishant-Jain-Codes@users.noreply.github.com>
2024-08-12 20:51:57 +0530

    Time: 12 ms (15.1%), Space: 18.2 MB (20.59%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
+        return dp_sol(prices);
         vector<vector<int>> dp(prices.size(),vector<int>(2,-1));
         return recursion(prices,0,true,dp);
     }
@@ -24,5 +25,26 @@ private :
             ans =  max(r1,r2);
         }
         return dp[idx][canBuy] = ans;
+    }
+private: 
+    int dp_sol(vector<int> & prices){
+        vector<vector<int>> dp(prices.size()+1,vector<int>(2,0));
+        for(int idx = prices.size()-1;idx>=0;idx--){
+            for(int canBuy = 0 ; canBuy<=1 ; canBuy++){
+                int ans = 0;
+                if(canBuy){
+                    int r1 = dp[idx+1][canBuy];
+                    int r2 = dp[idx+1][!canBuy] - prices[idx];
+                    ans = max(r1,r2);
+                }
+                else{
+                    int r1 = dp[idx+1][canBuy];
+                    int r2 = dp[idx+1][!canBuy] + prices[idx];
+                    ans = max(r1,r2);
+                }
+                dp[idx][canBuy] = ans;
+            }
+        }
+        return dp[0][1];
     }
 };
