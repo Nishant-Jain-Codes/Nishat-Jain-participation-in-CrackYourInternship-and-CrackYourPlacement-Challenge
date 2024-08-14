036b6f1d4e3ef31c0e6da44ebef5736a4176048c
nishant jain <83751330+Nishant-Jain-Codes@users.noreply.github.com>
2024-08-14 22:45:05 +0530

    Time: 4 ms (56.1%), Space: 14.9 MB (31.06%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -1,27 +1,15 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        //find first decreasing idx
         int right = nums.size()-2;
-        while(right>=0)
-        {
-            if(nums[right+1]>nums[right])
-                break;
+        while(right>=0 && nums[right+1]<= nums[right])
             right--;
+        if(right>=0){
+            int eleIdx = nums.size()-1;
+            while(nums[eleIdx]<=nums[right])
+                eleIdx--;
+            swap(nums[right],nums[eleIdx]);
         }
-        cout<<right;
-        if(right == -1)
-        {
-            sort(nums.begin(),nums.end());
-        }
-        else{
-            int eleIdx = -1;
-            for(int i = right+1; i<nums.size();i++){
-                if(nums[i]>nums[right] && (eleIdx == -1 || nums[eleIdx]>nums[i]))
-                    eleIdx = i;
-            }
-            swap(nums[eleIdx],nums[right]);
-            sort(nums.begin()+right+1,nums.end());
-        }
+        reverse(nums.begin()+right+1,nums.end());
     }
 };
