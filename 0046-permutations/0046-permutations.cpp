b01c15f389faa25e5794166abc570534e4087672
nishant jain <83751330+Nishant-Jain-Codes@users.noreply.github.com>
2024-08-14 22:23:06 +0530

    Time: 0 ms (100%), Space: 9.1 MB (57.52%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -1,13 +1,37 @@
 class Solution {
 public:
     vector<vector<int>> permute(vector<int>& nums) {
+        return method2(nums);
+        // return method1(nums)
+    }
+private: 
+    //time : O(n*n!)
+    //space : O(n) 
+    vector<vector<int>> method2(vector<int>&nums){
+        vector<vector<int>> allPer;
+        generatePermutations(0,nums,allPer);
+        return allPer;
+    }
+    void generatePermutations(int idx , vector<int>&nums, vector<vector<int>>& allPers){
+        if(idx == nums.size())
+        {allPers.push_back(nums);
+        return;}
+        for(int i = idx ; i<nums.size();i++){
+            swap(nums[idx],nums[i]);
+            generatePermutations(idx+1,nums,allPers);
+            swap(nums[i],nums[idx]);
+        }
+    }
+private: 
+    //time : O(n*n!)
+    //space : O(n!)
+    vector<vector<int>> method1(vector<int>&nums){
         vector<int> curPer;
         vector<vector<int>> allPer;
         unordered_set<int> takenIdx;
         doPermutations(nums,curPer,allPer,takenIdx);
         return allPer;
     }
-private: 
     void doPermutations(vector<int>& nums , vector<int> & curPer , vector<vector<int>> & allPer , unordered_set<int> &takenIdx){
         if(curPer.size() == nums.size())
         {
