8de5da88132a5ac4793879efa8d21c3fc0722a67
nishant jain <83751330+Nishant-Jain-Codes@users.noreply.github.com>
2024-08-16 04:34:06 +0530

    Time: 0 ms (100%), Space: 9.7 MB (8.53%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,11 +1,34 @@
 class Solution {
 public:
     int rob(vector<int>& nums) {
+        return iterative(nums);
         int n = nums.size();
         vector<vector<int>> dp(n+1,vector<int>(2,-1));
         return recursion(0,true,nums,dp);
     }
 private:
+    //time : O(2*N)
+    //space : O(2*N)
+    int iterative(vector<int>& nums){
+        int n = nums.size();
+        vector<vector<int>> dp(n+1,vector<int>(2,0));
+        int ans;
+        for(int idx = n-1;idx>=0;idx--){
+            for(int canRob = 0 ; canRob<=1 ; canRob++){
+                if(canRob){
+                    int rob = dp[idx+1][0] + nums[idx];
+                    int notRob = dp[idx+1][1];
+                    ans = max(rob,notRob);
+                }
+                else{
+                    int notRob = dp[idx+1][1];
+                    ans = notRob;
+                }
+                dp[idx][canRob] = ans;
+            }
+        }
+        return dp[0][1];
+    }
     //time : O(2*N)
     //space : O(2*N) + O(N) 
     int recursion(int idx , bool canRob , vector<int> & nums , vector<vector<int>> & dp){
