d539d296ac61db956c48c7cf19dd400bec013667
nishant jain <83751330+Nishant-Jain-Codes@users.noreply.github.com>
2024-08-18 10:37:39 +0530

    Time: 15 ms (53.04%), Space: 12.1 MB (45.92%) - LeetHub

---
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        int l = 0;
+        int r = 0;
+        unordered_map<int,int> m;
+        int size = 0;
+        int ans = 0;
+        while(r<s.length()){
+            m[s[r]]++;
+            if(m[s[r]]==1)
+                size++;
+            r++;
+            if(size==(r-l)){
+                ans = max(ans,size);
+            }
+            else{
+                while(size!=(r-l)){
+                    m[s[l]]--;
+                    if(m[s[l]]==0)
+                        size--;
+                    l++;
+                }
+            }
+        }
+        return ans;
+    }
+};
