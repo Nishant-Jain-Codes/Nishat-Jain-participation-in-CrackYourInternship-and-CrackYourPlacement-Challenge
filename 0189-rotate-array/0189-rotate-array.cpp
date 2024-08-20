cb579c7e10df7e93793d88d8834142d9c78490c6
nishant jain <83751330+Nishant-Jain-Codes@users.noreply.github.com>
2024-08-20 22:39:08 +0530

    Time: 21 ms (53.66%), Space: 27.5 MB (68.58%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -5,17 +5,7 @@ public:
             return;
         k=k%nums.size();
         reverse(nums.begin(),nums.end());
-        // print(nums);
         reverse(nums.begin(),nums.begin()+k);
-        // print(nums);
         reverse(nums.begin()+k,nums.end());
-        // print(nums);
-    }
-private: 
-    void print(vector<int>& v){
-    
-        for(auto &x: v)
-            cout<<x<<" ";
-        cout<<endl;
     }
 };
