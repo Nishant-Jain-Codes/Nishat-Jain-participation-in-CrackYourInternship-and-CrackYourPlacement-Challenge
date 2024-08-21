1b2b55551a2d3702c61501c13ea00a4b11af029f
nishant jain <83751330+Nishant-Jain-Codes@users.noreply.github.com>
2024-08-22 02:35:32 +0530

    Time: 198 ms (38.63%), Space: 122.1 MB (72.82%) - LeetHub

--- 2816-double-a-number-represented-as-a-linked-list/2816-double-a-number-represented-as-a-linked-list.cpp
+++ 2816-double-a-number-represented-as-a-linked-list/2816-double-a-number-represented-as-a-linked-list.cpp
@@ -10,26 +10,42 @@
  */
 class Solution {
 public:
+    ListNode* method2(ListNode* head){
+        
+        ListNode* newHead = new ListNode(0,head);
+        head = newHead;
+
+        for(ListNode* cur = head;cur!=NULL;cur=cur->next){
+            int val = (2*(cur->val))%10;
+            if(cur->next && cur->next->val>=5)
+                val++;
+            cur->val = val;
+        }
+        if(head->val == 0)
+            head = head->next;
+        return head;
+    }
     ListNode* doubleIt(ListNode* head) {
+        return method2(head);
         ListNode* prev = NULL;
         int carry = 0;
-        helper(head,carry);
-        if(carry)
-        {
+        helper(head, carry);
+        if (carry) {
             ListNode* newHead = new ListNode(carry);
-            newHead ->next = head;
+            newHead->next = head;
             head = newHead;
         }
         return head;
     }
-private: 
-    void helper(ListNode* cur,int & carry){
-        if(!cur)
-            return ;
-        helper(cur->next,carry);
-        int val = 2*(cur->val) + carry;
-        carry = val/10;
-        val = val%10;
+
+private:
+    void helper(ListNode* cur, int& carry) {
+        if (!cur)
+            return;
+        helper(cur->next, carry);
+        int val = 2 * (cur->val) + carry;
+        carry = val / 10;
+        val = val % 10;
         cur->val = val;
         // return cur;
     }
