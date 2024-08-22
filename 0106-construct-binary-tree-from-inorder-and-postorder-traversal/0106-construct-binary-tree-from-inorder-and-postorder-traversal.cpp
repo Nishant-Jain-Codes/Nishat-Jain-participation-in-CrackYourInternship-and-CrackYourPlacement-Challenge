d8682923fb9d6151f65789788d638154615b47e2
nishant jain <83751330+Nishant-Jain-Codes@users.noreply.github.com>
2024-08-22 17:56:41 +0530

    Time: 18 ms (23.49%), Space: 26.5 MB (66.81%) - LeetHub

--- 0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.cpp
+++ 0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.cpp
@@ -6,44 +6,47 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
 public:
-    TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder)
-    {
-        return builder(postorder,0,postorder.size()-1,inorder,0,inorder.size()-1);    
+    TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
+        return builder(postorder, 0, postorder.size() - 1, inorder, 0,
+                       inorder.size() - 1);
     }
+
 private:
-    TreeNode* builder(vector<int>& post,int pS,int pE,vector<int>& ino,int iS,int iE){
-        if(pS>pE)
+    TreeNode* builder(vector<int>& post, int pS, int pE, vector<int>& ino,
+                      int iS, int iE) {
+        if (pS > pE)
             return NULL;
-        if(pS==pE)
+        if (pS == pE)
             return new TreeNode(post[pE]);
         int rootVal = post[pE];
         TreeNode* root = new TreeNode(rootVal);
         int rootIndexInInorder;
         int i = iS;
-        while(i<=iE)
-        {
-            if(ino[i]==rootVal)
+        while (i <= iE) {
+            if (ino[i] == rootVal)
                 break;
             i++;
         }
         rootIndexInInorder = i;
-        // cout<<rootIndexInInorder<<" "<<ino[rootIndexInInorder]<<" "<<rootVal<<endl;
+        // cout<<rootIndexInInorder<<" "<<ino[rootIndexInInorder]<<"
+        // "<<rootVal<<endl;
         int iSL = iS;
-        int iEL = rootIndexInInorder-1;
-        int leftSubtreeSize = iEL-iSL+1;
-        int iSR = rootIndexInInorder+1;
+        int iEL = rootIndexInInorder - 1;
+        int leftSubtreeSize = iEL - iSL + 1;
+        int iSR = rootIndexInInorder + 1;
         int iER = iE;
         int pSL = pS;
-        int pEL = pSL+leftSubtreeSize-1;
+        int pEL = pSL + leftSubtreeSize - 1;
         int pSR = pEL + 1;
-        int pER = pE-1;
-        root->left = builder(post,pSL,pEL,ino,iSL,iEL);
-        root->right = builder(post,pSR,pER,ino,iSR,iER);
+        int pER = pE - 1;
+        root->left = builder(post, pSL, pEL, ino, iSL, iEL);
+        root->right = builder(post, pSR, pER, ino, iSR, iER);
         return root;
     }
 };
