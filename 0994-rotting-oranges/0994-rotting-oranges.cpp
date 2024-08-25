d8b5c287f506e87f449d90929a37979cd912be54
nishant jain <83751330+Nishant-Jain-Codes@users.noreply.github.com>
2024-08-25 13:24:29 +0530

    Time: 19 ms (5.76%), Space: 21.8 MB (5.12%) - LeetHub

---
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    int orangesRotting(vector<vector<int>>& grid) {
+        int m = grid.size();
+        int n = grid[0].size();
+        vector<vector<int>> minTime(m,vector<int>(n,INT_MAX));
+        vector<pair<int,int>> dir = {{0,1},{0,-1},{1,0},{-1,0}};
+        for(int i = 0;i<m;i++){
+            for(int j = 0;j<n;j++){
+                if(grid[i][j]==2){
+                    vector<vector<int>> visited(m,vector<int>(n,0));
+                    queue<vector<int>> q;
+                    q.push({i,j,0});
+                    visited[i][j]=1;
+                    while(!q.empty()){
+                        int x = q.front()[0];
+                        int y = q.front()[1];
+                        int t = q.front()[2];
+                        q.pop();
+                        minTime[x][y] = min(minTime[x][y],t);
+                        for(auto & d : dir){
+                            int x_ = x + d.first;
+                            int y_ = y + d.second;
+                            if(x_>=0 && y_>=0 && x_<m && y_<n && !visited[x_][y_] && grid[x_][y_]==1){
+                                visited[x_][y_] = 1;
+                                q.push({x_,y_,t+1});
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        int ans = 0;
+        for(int i = 0;i<m;i++){
+            for(int j = 0 ; j<n;j++){
+                if(grid[i][j]!=0)
+                    if(minTime[i][j]==INT_MAX)
+                        return -1;
+                    else 
+                        ans = max(minTime[i][j],ans);
+
+            }
+        }
+        return ans;
+    }   
+};
