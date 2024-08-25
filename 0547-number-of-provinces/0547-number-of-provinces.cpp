be0564cfacc6c6eef949ac515fa37bedfdbac1f5
nishant jain <83751330+Nishant-Jain-Codes@users.noreply.github.com>
2024-08-25 07:31:15 +0530

    Time: 12 ms (97.78%), Space: 18.9 MB (55.36%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,18 +1,33 @@
 class Solution {
 public:
     int findCircleNum(vector<vector<int>>& isConnected) {
-        // return bfs(isConnected);
         vector<int> visited(isConnected.size(),0);
         int ans = 0;
         for(int i = 0;i<isConnected.size();i++){
             if(!visited[i]){
                 ans++;
-                dfs(i,isConnected,visited);
+                bfs(i,isConnected,visited);
+                // dfs(i,isConnected,visited);
             }
         }
         return ans;
     }
 private: 
+    void bfs(int curV,vector<vector<int>>& graph ,vector<int>& visited){
+        queue<int> q;
+        q.push(curV);
+        visited[curV]=1;
+        while(!q.empty()){
+            int nowV = q.front();
+            q.pop();
+            for(int adjV = 0;adjV<graph.size();adjV++){
+                if(!visited[adjV] && graph[nowV][adjV]){
+                    visited[adjV]=1;
+                    q.push(adjV);
+                }
+            }
+        }
+    }
     void dfs(int curV,vector<vector<int>>& graph ,vector<int>& visited){
         visited[curV] = 1;
         for(int adjV = 0 ; adjV<graph[0].size();adjV++){
