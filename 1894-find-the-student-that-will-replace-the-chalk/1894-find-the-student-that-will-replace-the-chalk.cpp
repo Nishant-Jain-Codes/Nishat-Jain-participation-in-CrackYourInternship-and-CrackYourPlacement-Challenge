b6683be1c577b65c5ee7b2d1d88f279ed900485f
nishant jain <83751330+Nishant-Jain-Codes@users.noreply.github.com>
2024-09-02 10:19:27 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 1894-find-the-student-that-will-replace-the-chalk/1894-find-the-student-that-will-replace-the-chalk.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int chalkReplacer(vector<int>& chalk, int k) {
+        int useInOneItration = 0;
+        for(auto & x : chalk)
+            useInOneItration += x;
+        if(k>=useInOneItration)
+            k%=useInOneItration;
+        int i = 0;
+        while(true){
+            if(k< chalk[i])
+                return i;
+            k-=chalk[i];
+            i++;
+            i%=chalk.size();
+        }
+    }
+};
